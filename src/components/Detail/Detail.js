9c4ee63e6f2980759842b78cd67cfef44994b501
Kellen Baker <mkellenbaker@gmail.com>
2019-06-22 12:07:02 -0500

    back button functional

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -3,10 +3,17 @@ import { connect } from "react-redux";
 
 
 class Detail extends Component {
+
+    handleBack = () => {
+        this.props.history.push('/');
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handleBack}>Back to List</button>
+                <button>Edit</button>
                 <h1>{this.props.reduxState.lastClicked.title}</h1>
+                <p>{this.props.reduxState.lastClicked.description}</p>
 
             </div>
         )
